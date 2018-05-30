41ab00d38b60eeac906db4d41e7039738fd9e94d
Foad Ardalan <foad.ardalan@outlook.fr>
2018-05-30 10:28:57 +0200

    clean

--- src/modules/authentication/authentication.controller.ts
+++ src/modules/authentication/authentication.controller.ts
@@ -40,4 +40,52 @@ export class AuthenticationController {
       .send());
   }
 
+  @Post('signout')
+  public async signout() {
+
+  }
+
+  @Post('forgot')
+  public async forgot() {
+
+  }
+
+  @Post('reset')
+  public async reset() {
+
+  }
+
 }
+
+/**
+ *
+ *
+ *
+  var users = require('../controllers/users.server.controller');
+
+  // Setting up the users password api
+  app.route('/api/auth/forgot').post(users.forgot);
+  app.route('/api/auth/reset/:token').get(users.validateResetToken);
+  app.route('/api/auth/reset').post(users.reset);
+
+  app.route('/api/auth/signup').post(users.signup)
+  app.route('/api/auth/signin').post(passport.authenticate('local'), users.signin)
+  app.route('/api/auth/signout').post(users.signout)
+
+  // Jwt token
+  app.route('/api/auth/token').post(users.token)
+  // Jwt protected route example:
+  // app.route('/api/auth/secretPlace').get(passport.authenticate('jwt'), (req, res) => {
+  //   console.log(req.user)
+  //   console.log(req.isAuthenticated())
+  //   res.status(200).send()
+  // })
+
+  // Setting the oauth routes
+  app.route('/api/auth/:strategy').get(users.oauthCall)
+  app.route('/api/auth/:strategy/callback').get(users.oauthCallback)
+ *
+ *
+ *
+ *
+ */
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -18,31 +18,87 @@ export class UsersController {
 
   constructor(private readonly usersService: UsersService) { }
 
+  @Get('me')
+  public async getCurrentUser(@Request() request, @Param('id', new ParseIntPipe()) id) {
+    return request.user;
+  }
+
+  @Put()
+  public async modifyUser(@Request() request, @Param('id', new ParseIntPipe()) id) {
+    return request.user;
+  }
+/*
+  @Delete('accounts')
+  public async deleteUser(@Request() request, @Param('id', new ParseIntPipe()) id) {
+    return request.user;
+  }
+
+  @Post('accounts')
+  public async addUser(@Request() request, @Param('id', new ParseIntPipe()) id) {
+    return request.user;
+  }
+*/
+  @Post('password')
+  public async changePassword(@Request() request, @Param('id', new ParseIntPipe()) id) {
+    return request.user;
+  }
+
+  @Post('picture')
+  public async changePicture(@Request() request, @Param('id', new ParseIntPipe()) id) {
+    return request.user;
+  }
+
+  @Get()
+  @Roles('admin')
+  public async getAllUsers() {
+    return this.usersService.getAll();
+  }
+/*
   @Post()
   @Roles('admin')
   @UsePipes(new ValidationPipe())
   public async addUser(@Body('user') user) {
     return this.usersService.add(user);
   }
-
-  @Get()
-  public async getAllUsers() {
-    return this.usersService.getAll();
-  }
+*/
 
   @Get(':id')
   public async getUser(@Request() request, @Param('id', new ParseIntPipe()) id) {
     return request.user;
   }
-
+/*
   @Put(':id')
   @UsePipes(new ValidationPipe())
-  public async replaceUser(@Request() request, @Body('user') user) {
+  public async modifyUser(@Request() request, @Body('user') user) {
     return this.usersService.update(request.user);
   }
-
+*/
   @Delete(':id')
   public async deleteUser(@Request() request, @Param('id', new ParseIntPipe()) id) {
     return this.usersService.remove(request.user);
   }
 }
+
+/**
+ *   // Setting up the users profile api
+  app.route('/api/users/me').get(passport.authenticate('jwt'), users.me);
+  app.route('/api/users').put(passport.authenticate('jwt'), users.update);
+  app.route('/api/users/accounts').delete(users.removeOAuthProvider);
+  app.route('/api/users/accounts').post(users.addOAuthProviderUserProfile);
+  app.route('/api/users/password').post(users.changePassword);
+  app.route('/api/users/picture').post(users.changeProfilePicture);
+
+  // Finish by binding the user middleware
+  app.param('userId', users.userByID);
+
+   app.route('/api/users').get(passport.authenticate('jwt'), adminPolicy.isAllowed, admin.list);
+
+  // Single user routes
+  app.route('/api/users/:userId')
+    .get(adminPolicy.isAllowed, admin.read)
+    .put(passport.authenticate('jwt'), adminPolicy.isAllowed, admin.update)
+    .delete(passport.authenticate('jwt'), adminPolicy.isAllowed, admin.delete);
+
+  // Finish by binding the user middleware
+  app.param('userId', admin.userByID);
+ */
